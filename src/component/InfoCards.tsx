37cdabe1771c1c0535187b93310d27b7f734e02a
didaryalcin <ddrylcn@gmail.com>
2024-06-02 18:54:09 +0300

    Loading...

--- src/component/InfoCards.tsx
+++ src/component/InfoCards.tsx
@@ -17,19 +17,19 @@ const InfoCards: React.FC<InfoCardsProps> = ({ info }) => {
         <div className="info-cards">
             <div className="info-card">
                 <h2>Subscription expires on:</h2>
-                <p>{info ? info.subscriptionExpires : 'Loading...'}</p>
+                <p>{info ? info.subscriptionExpires : 'July 23, 2023'}</p>
             </div>
             <div className="info-card">
                 <h2>Last charge:</h2>
-                <p>{info ? info.lastCharge : 'Loading...'}</p>
+                <p>{info ? info.lastCharge : '$50 on June 23, 2023'}</p>
             </div>
             <div className="info-card">
                 <h2>Total Usage Data:</h2>
-                <p>{info ? info.totalUsageData : 'Loading...'}</p>
+                <p>{info ? info.totalUsageData : '36.025 GB'}</p>
             </div>
             <div className="info-card">
                 <h2>Daily Usage Data:</h2>
-                <p>{info ? info.dailyUsageData : 'Loading...'}</p>
+                <p>{info ? info.dailyUsageData : '1.025 GB'}</p>
             </div>
         </div>
     );
